50de71dabab5c8536d56c9bb0009a0e84e29710a
paulozasimowicz <pvzpcp@gmail.com>
2025-01-15 16:53:06 +0100

    Add more examples

--- Phyton_Exercises/Example_01.py
+++ Phyton_Exercises/Example_01.py
@@ -3,3 +3,28 @@ for i in range(3):
     #Statement that gets repeated
     print("Hello")
 
+#Loop using 1 as internal variable
+for i in range(3):
+    print(i)
+
+print("--") #This is just a separator
+
+#Loop using "something" as internal variable
+for something in range(3):
+    print(something)
+
+seats = 500 #initial number of seats
+while seats > 0: #seat avaiable?
+    print("Sell ticket") # ticket sold
+    seats = seats - 1 # number of seats updated
+
+for box in range(50):
+  print("Package A")
+  print("Task complete")
+
+age = 30
+if age >= 18:
+  print("Regular price")
+else:
+  print("Discount")
+print("Proceed to payment")
