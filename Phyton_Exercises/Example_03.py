628a81e1549d1d0fb03681861b21e5cdfe1ffe0a
paulozasimowicz <pvzpcp@gmail.com>
2025-01-16 15:44:38 +0100

    New exercise

--- Phyton_Exercises/Example_03.py
+++ Phyton_Exercises/Example_03.py
@@ -11,3 +11,21 @@ print(item2)
 
 
 print("Bee".find("e"))
+
+#Lenght function: len(): len() stands for length and, when used on lists, it returns the number of items in the list.
+
+movies = ["avatar","Titanic","avengers"]
+print(len(movies))
+
+# append() dunction: The append() function adds a new item to the end of a list. append() is called using dot notation because it’s specific to lists.
+
+songs = ["Yesterday", "Hello","Believer"]
+songs.append("imagine")
+print(songs)
+
+# Insert function: The insert() function allows you to add an element to a list, at a specific position.
+
+items = ["book","pencil","fridge"]
+items.insert(2,"coisa")
+print(items)
+print(items[2])
---
+++ Phyton_Exercises/Exemple_04.py
@@ -0,0 +1,5 @@
+# Custom functions
+
+def greet(): 
+  print("Hello from a function")
+  print("Have a great day")
