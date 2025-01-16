2fa388e3bfa6ce7674c2b5c311a15ce7fa92f042
paulozasimowicz <pvzpcp@gmail.com>
2025-01-16 11:17:42 +0100

    Adding new exercise

---
+++ Phyton_Exercises/Example_03.py
@@ -0,0 +1,13 @@
+# Strings
+print('HGvweonvwrjnK'.upper())
+print('HGvweonvwrjnK'.lower())
+print('cap'.capitalize())
+
+item = "smartwatch"
+print(item.upper())
+print(item) # original string
+item2 = item.upper()
+print(item2)
+
+
+print("Bee".find("e"))
