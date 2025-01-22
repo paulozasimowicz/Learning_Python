2d73990bc24b2d8039159a3231710361e669f75a
paulozasimowicz <pvzpcp@gmail.com>
2025-01-22 14:21:20 +0100

    new exercise

--- Phyton_Exercises/Example_06.py
+++ Phyton_Exercises/Example_06.py
@@ -22,3 +22,28 @@ print(triple(5))
 
 # The map() function applies a specified function to every element in an iterable, like lists or tuples.
 #  It produces a result that can be transformed into a list using the list() function for easy viewing or further use.
+
+def total(numbers):
+    result = 0
+    # iterating over the list
+    for i in numbers:
+        result+=i
+    return result
+nums = [1,2,3,4,5]
+print(total(nums))
+
+def total(*args):
+    result = 0
+    for arg in args:
+        result+=arg
+    return result
+print(total(1,2,3,4))
+print(total(1,2,3,4,5,6,7))
+print(total(1,2,3,4,5,6,7,8))
+
+#kwarg is a dictionary
+def display_info(**kwargs):
+    #kwargs.items() return the key:valie pairs
+    for key, value in kwargs.items():
+        print(key,":",value)
+display_info(name="Alice",age="30",city="New York")
